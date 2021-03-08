6dfb4abe496f832a6f7ba6647807ef89b483f699
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-08 22:18:49 +1100

    store products list screen

---
+++ ShoeStore-Frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from "./store/store.module";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
